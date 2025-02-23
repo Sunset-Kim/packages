7b6d0002ca9b46de67e15a8cf733ab271c1a8b37
김민우 <0chickim@gmail.com>
2025-02-23 23:25:26 +0900

    feat: first lecture - npm package를 수동으로 만드는 방법

---
+++ rollup.config.js
@@ -0,0 +1,20 @@
+import typescript from "rollup-plugin-typescript2";
+import del from "rollup-plugin-delete";
+
+export default {
+  input: "src/index.ts",
+  output: [
+    {
+      file: "dist/index.cjs",
+      format: "cjs",
+    },
+    {
+      file: "dist/index.esm.js",
+      format: "esm",
+    },
+  ],
+  plugins: [
+    del({ targets: ["dist/*"] }),
+    typescript({ useTsconfigDeclarationDir: true }),
+  ],
+};
